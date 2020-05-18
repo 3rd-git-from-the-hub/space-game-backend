833b2875474e0431d6a77244d3d9a86bad2b5aeb
Erik Ford <ekford18@gmail.com>
2020-05-18 11:10:10 -0700

    seperated data tables into locations and events

---
+++ data/locations.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    location_name: 'Gilles C11Y',
+    location_type: 'Planet',
+    location_image: 'image',
+    location_description: 'Gilles C11Y is a desert of smooth rocks. Spires of sharp stone interrupt the otherwise flat terrain. There are groups of tall, dark orange stalks everywhere. There seems to be large burrows throughout the seemingly deserted planet.',
+    been_visited: false,
+    location_id: 1
+  },
+
+  {
+    location_name: 'Sulia V',
+    location_type: 'Planet',
+    location_image: 'image',
+    location_description: 'Dense forests of brown plants cover Sulia V. Large water-filled caves are present just beneath the surface. The geological record shows irregular periods where, for unknown reasons, all oxygen vanished from the atmosphere.',
+    been_visited: false,
+    location_id: 2,
+  },
+  {
+    location_name: 'Ipelup VI',
+    location_type: 'Planet',
+    location_image: 'image',
+    location_description: 'A planet-wide, deep ocean. The planet is home to extremely dangerous apex predators. The stone of the planetary crust is unusually dense and hard.',
+    been_visited: false,
+    location_id: 3
+  },
+
+  {
+    location_name: 'Selelup II',
+    location_type: 'Planet',
+    location_image: 'image',
+    location_description: 'Selelup II is a rocky desert type planet that is lit by a gentle sun. Spires of sharp stone interrupt the otherwise flat eterrain. There are signs of a once foreign colony, but now it seems abandoned. There is no water on this now deserted planet.',
+    been_visited: false,
+    location_id: 4
+  }
+];   
+        
