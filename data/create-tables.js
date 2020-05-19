3ba48811331c3662d2f4ab89c99029d2b74f167c
Erik Ford <ekford18@gmail.com>
2020-05-19 14:50:24 -0700

    repush

--- data/create-tables.js
+++ data/create-tables.js
@@ -16,7 +16,6 @@ async function run() {
                     id SERIAL PRIMARY KEY,
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL,
-                    game_id INTEGER,
                     user_ship VARCHAR(512)
 
                 );           
--- data/users.js
+++ data/users.js
@@ -1,17 +1,5 @@
 module.exports = [
   {
-    email: 'john@arbuckle.com',
-    hash: 1234,
-    user_ship: {
-      ship_name: 'The Enterprise',
-      ship_image: 'im an image',
-      ship_fuel: 4,
-      ship_hull: 10,
-      base_combat: 2,
-      base_diplomacy: 4,
-      base_science: 2,
-      used_item_slots: 0,
-      max_item_slots: 3
-    },
+    
   }
 ];
