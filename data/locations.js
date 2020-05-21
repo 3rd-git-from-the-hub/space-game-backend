7bc544cdef8dd35a920609c7fabe7a99827bc095
Erik Ford <ekford18@gmail.com>
2020-05-21 15:14:39 -0700

    pushing to dev

--- data/locations.js
+++ data/locations.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     location_name: 'Gilles C11Y',
     location_type: 'Planet',
-    location_image: 'image',
+    location_image: 'https://cdn.pixabay.com/photo/2018/08/08/20/23/planet-3593060_960_720.jpg',
     location_description: 'Gilles C11Y is a desert of smooth rocks. Spires of sharp stone interrupt the otherwise flat terrain. There are groups of tall, dark orange stalks everywhere. There seems to be large burrows throughout the seemingly deserted planet.',
     event_id: 1
   },
@@ -10,14 +10,14 @@ module.exports = [
   {
     location_name: 'Sulia V',
     location_type: 'Planet',
-    location_image: 'image',
+    location_image: 'https://cdn.pixabay.com/photo/2017/07/11/21/21/planet-2495089_960_720.jpg',
     location_description: 'Dense forests of brown plants cover Sulia V. Large water-filled caves are present just beneath the surface. The geological record shows irregular periods where, for unknown reasons, all oxygen vanished from the atmosphere.',
     event_id: 2
   },
   {
     location_name: 'Ipelup VI',
     location_type: 'Planet',
-    location_image: 'image',
+    location_image: 'https://cdn.pixabay.com/photo/2020/05/04/14/55/sci-fi-5129504_960_720.jpg',
     location_description: 'A planet-wide, deep ocean. The planet is home to extremely dangerous apex predators. The stone of the planetary crust is unusually dense and hard.',
     event_id: 3
   },
@@ -25,9 +25,8 @@ module.exports = [
   {
     location_name: 'Selelup II',
     location_type: 'Planet',
-    location_image: 'image',
+    location_image: 'https://cdn.pixabay.com/photo/2018/08/12/00/48/planet-3599887_960_720.jpg',
     location_description: 'Selelup II is a rocky desert type planet that is lit by a gentle sun. Spires of sharp stone interrupt the otherwise flat eterrain. There are signs of a once foreign colony, but now it seems abandoned. There is no water on this now deserted planet.',
     event_id: 4
   }
 ];   
-        
--- data/shipchoices.js
+++ data/shipchoices.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    ship_name: 'The Enterprise',
+    ship_name: 'diplomacy',
     ship_image: 'im an image',
     ship_fuel: 4,
     ship_hull: 10,
@@ -12,7 +12,19 @@ module.exports = [
     planet_visited: []
   },
   {
-    ship_name: 'The Enterprise2',
+    ship_name: 'combat',
+    ship_image: 'im an image',
+    ship_fuel: 4,
+    ship_hull: 10,
+    base_combat: 2,
+    base_diplomacy: 4,
+    base_science: 2,
+    used_item_slots: 0,
+    max_item_slots: 3,
+    planet_visited: []
+  },
+  {
+    ship_name: 'science',
     ship_image: 'im an image',
     ship_fuel: 4,
     ship_hull: 10,
